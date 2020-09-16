de372af2e8aef577edeb010eba244045a8dbb524
joseph <josephjohndemarco@gmail.com>
2020-09-16 14:28:26 -0400

    Add toy with python3 manage.py shell. Add import for model. Update view to display all toys.

--- main_app/models.py
+++ main_app/models.py
@@ -10,3 +10,11 @@ class Toy(models.Model):
 
     def __str__(self):
         return self.name
+
+'''
+toys = [
+  Toy('Attack Armor Batman', 'Mattel', 'Batman superhero action figure', 2004, 400),
+  Toy('Scratch the Cat', 'Playmates', 'TMNT minor character Scratch', 1993, 1500),
+  Toy('Green Beret G.I. Joe', 'Hasbro', 'American hero toy G.I. Joe', 1966, 2100)
+]
+'''
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Toy
 
 # Create your views here.
 def home(request):
@@ -9,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def toys_index(request):
+    toys = Toy.objects.all()
     return render(request, 'toys/index.html', {'toys' : toys})
