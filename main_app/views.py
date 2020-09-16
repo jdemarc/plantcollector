23c8f63aa0357cb2ba7fc3be04cd73a4b20c59f6
joseph <josephjohndemarco@gmail.com>
2020-09-16 14:22:42 -0400

    Remove simulated model and add Toy model. makemigrations and migrate changes.

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Simulating models...
-class Toy:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, maker, description, release_year, value):
-    self.name = name
-    self.maker = maker
-    self.description = description
-    self.release_year = release_year
-    self.value = value
-
-toys = [
-  Toy('Attack Armor Batman', 'Mattel', 'Batman superhero action figure', 2004, 400),
-  Toy('Scratch the Cat', 'Playmates', 'TMNT minor character Scratch', 1993, 1500),
-  Toy('Green Beret G.I. Joe', 'Hasbro', 'American hero toy G.I. Joe', 1966, 2100)
-]
-
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Hello.</h1>')
