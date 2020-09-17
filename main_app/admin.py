5683e4528e56cf33ed8cf3889e29fd571c48bcdd
joseph <josephjohndemarco@gmail.com>
2020-09-16 20:25:42 -0400

    Register Maintaining model in admin.py.

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Toy
+from .models import Toy, Maintaining
 
 # Register your models here.
 admin.site.register(Toy)
+admin.site.register(Maintaining)
