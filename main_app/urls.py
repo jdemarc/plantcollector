5b697913f0cefb4d3dea71cda0fbce616693e9d1
joseph <josephjohndemarco@gmail.com>
2020-09-16 20:51:25 -0400

    Add script to details page to properly render materialize js. Add route to add maintenance. Add function to add maintenance.

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+   path('toys/<int:toy_id>/add_maintenance/', views.add_maintenance, name='add_maintenance')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Toy
 from .forms import MaintainingForm
@@ -18,9 +18,16 @@ def toys_index(request):
 
 def toys_detail(request, toy_id):
   toy = Toy.objects.get(id=toy_id)
-
   maintaining_form = MaintainingForm()
   return render(request, 'toys/detail.html', {'toy': toy, 'maintaining_form': maintaining_form})
+
+def add_maintenance(request, toy_id):
+  form = MaintainingForm(request.POST)
+  if form.is_valid():
+    new_maintenance = form.save(commit=False)
+    new_maintenance.toy_id = toy_id
+    new_maintenance.save()
+  return redirect('detail', toy_id=toy_id)
 
 class ToyCreate(CreateView):
   model = Toy
