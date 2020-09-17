73255827f06f66e508b80be35238095d4e44ae10
joseph <josephjohndemarco@gmail.com>
2020-09-17 14:56:09 -0400

    Add insect routes for CRUD ops.

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
    path('plants/create/', views.PlantCreate.as_view(), name='plants_create'),
    path('plants/<int:pk>/update/', views.PlantUpdate.as_view(), name='plants_update'),
    path('plants/<int:pk>/delete/', views.PlantDelete.as_view(), name='plants_delete'),
-   path('plants/<int:plant_id>/add_watering/', views.add_watering, name='add_watering')
+   path('plants/<int:plant_id>/add_watering/', views.add_watering, name='add_watering'),
+   path('insects/', views.InsectList.as_view(), name='insects_index'),
+   path('insects/<int:pk>/', views.InsectDetail.as_view(), name='insects_detail'),
+   path('insects/create/', views.InsectCreate.as_view(), name='insects_create'),
+   path('insects/<int:pk>/update/', views.InsectUpdate.as_view(), name='insects_update'),
+   path('insects/<int:pk>/delete/', views.InsectDelete.as_view(), name='insects_delete'),
 ]
